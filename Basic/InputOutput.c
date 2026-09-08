c1cc9303ea09c6f3eb0ced8cb9728e4efe9a05a7
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-08 12:07:19 +0600

    Enhance InputOutput.c with important comments
    Added comments to indicate important input/output sections and variables.

--- Basic/InputOutput.c
+++ Basic/InputOutput.c
@@ -1,37 +1,73 @@
 /**
- * C programming basic input output
+ * C programming basic input/output
  */
  #include <stdio.h>
  int main(){
      /**
       * All primitive and derived types
       */
-      char charvalue;
+      char charvalue;  // important
       unsigned char uchar;
       short shortvalue;
       unsigned short ushort;
-      int intvalue;
+      int intvalue;  //important
       unsigned uint;
       long longvalue;
       unsigned long ulong;
-      long long longlongvalue;
+      long long longlongvalue;  //important
       unsigned long long ulonglong;
-      float floatvalue;
-      double doublevalue;
+      float floatvalue;      //important
+      double doublevalue;  //important
       long double longdouble;
       /**
        *Input secstion
        */
-       printf("Enter a character =");
+       printf("Enter a character ="); //important
        charvalue = getchar();    // getchar() function to input a character
+       
        printf("Enter one another character = ");
        uchar = getchar();
        
-       printf("Enter an signed shortvalue = ");
+       printf("Enter a signed shortvalue = ");
        scanf("%hi", &shortvalue);
        
-       printf("")
+       printf("Enter an unsigned shortvalue = ");
+       scanf("%hu", &ushort);
        
+       printf("Enter a signed integer = ");//important
+       scanf("%d", &intvalue);
+       
+       printf("Enter a unsigned integer value = ");
+       scanf("%lu", &uint);
+       
+       printf("Enter a signed long value = ");
+       scanf("%ld", &longvalue);
+       
+       printf("Enter a unsigned long value = ");
+       scanf("%lu", &ulong);
+       
+       printf("Enter a long long signed value =  ");//important
+       scanf("%lld", &longlongvalue);
+       
+       printf("Enter an unsigned long long value = ");
+       scanf("%llu", &ulonglong);
+       
+       printf("Enter a float value = ");//important
+       scanf("%f", &floatvalue);
+       
+       printf("Enter a double value = ");//important
+       scanf("%lf", &doublevalue);
+       
+       printf("Enter a long doublevalue = ");
+       scanf("%Lf", &longdouble);
+       
+       
+       /*
+       Print the value of all variable
+       */
+       
+       
+        
        
       
  }
