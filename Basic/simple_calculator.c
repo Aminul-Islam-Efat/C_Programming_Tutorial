fca40895a894343bdcb885c3dea5fd389c92db85
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-15 20:12:33 +0600

    Implement basic arithmetic operations in calculator

--- Basic/simple_calculator.c
+++ Basic/simple_calculator.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int main(){
+    int a,b,add,sub,mul,div;
+    printf("Enter 1st number = ");
+    scanf("%d",&a);
 
+    printf("Enter 2nd number = ");
+    scanf("%d",&b);
+
+   add = a+b;
+   sub = a-b;
+   mul = a*b;
+   div =  a/b;
+
+   printf("Addition = %d\n",add);
+   printf("subtraction = %d\n",sub);
+   printf("Multiplication = %d\n",mul);
+   printf("Division = %d\n",div);
+    return 0;
+}
