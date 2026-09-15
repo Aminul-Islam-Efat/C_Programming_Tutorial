f72c749ee941aaedeaeda85c23a8478b121c9720
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-15 21:46:35 +0600

    Update Even_or_odd.c

--- Basic/Even_or_odd.c
+++ Basic/Even_or_odd.c
@@ -1,1 +1,20 @@
 
+    /*
+    Even or odd number test.
+    */
+
+#include <stdio.h>
+int main(){
+
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+
+    if(num%2 == 0){
+        printf("The numberr is Even");
+    }else{
+        printf("The number is odd");
+    }
+    return 0;
+
+}
