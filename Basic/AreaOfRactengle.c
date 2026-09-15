c99c4e5be9813c0a3501ddc9ba5b2d0cbe7233fc
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-15 20:34:59 +0600

    Update AreaOfRactengle.c

--- Basic/AreaOfRactengle.c
+++ Basic/AreaOfRactengle.c
@@ -1,1 +1,21 @@
+/* 
+  Area of a rectangle 
+  Take length and width from the user.
+*/
 
+#include <stdio.h>
+int main(){
+    int len, wid, area;
+
+    printf("Enter  Length of the Rectangle : ");
+    scanf("%d",&len);
+
+    printf("Enter the  width Rectangle : ");
+    scanf("%d",&wid);
+
+     area = len * wid;
+
+    printf("Area of the ractangle : %d ",area);
+    return 0;
+
+}
