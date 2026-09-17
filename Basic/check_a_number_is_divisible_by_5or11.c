eb01c588a350bae26475f1bae692ad486fd83ebd
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-17 22:47:34 +0600

    Add program to check divisibility by 5 and 11

--- Basic/check_a_number_is_divisible_by_5or11.c
+++ Basic/check_a_number_is_divisible_by_5or11.c
@@ -1,1 +1,22 @@
+/*
+check a number is divisible by 5 & 11
+*/
 
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number = ");
+    scanf("%d",&num);
+
+    if(num % 11 == 0 && num % 5 == 0){
+        printf("%d the number divisible by both 5 and 11\n",num);
+    }else if(num % 11 == 0){
+        printf("%d is devisible by 11\n",num);
+    }else if(num % 5 == 0){
+        printf("%d is divisible by 5\n ",num);
+    }else {
+        printf("%d number is not divisible by 5 and 11\n",num);
+    }
+    return 0;
+}
