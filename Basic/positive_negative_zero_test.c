0d7fdfe5867a7f0655025d827483692cb4af000c
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-17 22:07:48 +0600

    Update positive_negative_zero_test.c

--- Basic/positive_negative_zero_test.c
+++ Basic/positive_negative_zero_test.c
@@ -1,1 +1,23 @@
+/*
+Check Largest number 
+*/
 
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter three numbers : ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (a == b && b == c) {
+        printf("All three numbers are same");
+    } else if (a >= b && a >= c) {
+        printf("%d is the largest number", a);
+    } else if (b >= a && b >= c) {
+        printf("%d is the largest number", b);
+    } else {
+        printf("%d is the largest number", c);
+    }
+
+    return 0;
+}
