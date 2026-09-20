0306a7b40de9d6f70535d1f9e2d99a6ea0ae5b88
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-20 11:56:52 +0600

    Update Electricity_Bill.c

--- Basic/Electricity_Bill.c
+++ Basic/Electricity_Bill.c
@@ -8,9 +8,9 @@ int main() {
     if (unit <= 100) {
         printf("Amount of money = %d", unit * 5);
     } else if (unit <= 200) {
-        printf("Amount of money = %d", (100 * 5) + ((unit - 100) * 8));
+        printf("Amount of money = %d", unit * 8));
     } else {
-        printf("Amount of money = %d", (100 * 5) + (100 * 8) + ((unit - 200) * 10));
+        printf("Amount of money = %d", unit * 10));
     }
 
     return 0;
