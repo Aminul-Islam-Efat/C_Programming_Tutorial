0d74967ede91605894c111edec465541293f8945
Aminul Islam Efat <aminulifu01@gmail.com>
2026-09-20 11:42:30 +0600

    Update Electricity_Bill.c

--- Basic/Electricity_Bill.c
+++ Basic/Electricity_Bill.c
@@ -1,18 +1,17 @@
-/*Electricity bill calculator*/
-
 #include <stdio.h>
 
-int main(){
+int main() {
     int unit;
     printf("Enter the unit = ");
-    scanf("%d",&unit);
-    if(unit <= 100){
-        printf("Amount of moeny = %d",unit*5);
-    }else if(unit < 200){
-        printf("Amount of money = %d",8*unit);
-    }else{
-        printf("Amount of money %d",10*unit);
-    }
-    return 0;
+    scanf("%d", &unit);
 
+    if (unit <= 100) {
+        printf("Amount of money = %d", unit * 5);
+    } else if (unit <= 200) {
+        printf("Amount of money = %d", (100 * 5) + ((unit - 100) * 8));
+    } else {
+        printf("Amount of money = %d", (100 * 5) + (100 * 8) + ((unit - 200) * 10));
+    }
+
+    return 0;
 }
